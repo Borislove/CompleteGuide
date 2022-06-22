430fecd870528eee5f7f86f410c06a62b47a8535
Borislove <abirme@yandex.ru>
2022-06-22 18:55:01 +0300

    base example

---
+++ src/chapter_2/Example2.java
@@ -0,0 +1,17 @@
+package chapter_2;
+
+/*Это еще один пример короткой программы.
+Присвоить исходному файлу имя "Example2.java"
+*/
+public class Example2 {
+    public static void main(String[] args) {
+        int num; // в этой строке кода объявляется (целочисленная) переменная с именем num
+        num = 100; //в этой строке кода переменной num присваивается значение 100
+        System.out.println("Это переменная num: " + num);
+
+        num = num * 2;
+
+        System.out.print("Значение переменной num * 2 равно ");
+        System.out.println(num);
+    }
+}
---
+++ src/chapter_2/Test.java
@@ -0,0 +1,19 @@
+package chapter_2;
+
+import java.io.UnsupportedEncodingException;
+
+public class Test {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        //System.setOut(new PrintStream(System.out, true, "cp866"));
+        //System.setOut(new PrintStream(System.out, true, "UTF-8"));
+
+        System.out.println("тест тест тест"); // TODO: 22.06.2022  console cmd (chcp 65001)
+
+        //-----------------------------------------
+       /* System.setOut(new PrintStream(System.out, true, "windows-1251"));
+        System.out.println("test test test");*/
+        //-----------------------------------------
+    }
+}
+
+
