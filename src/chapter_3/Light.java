bdfb4ad30a032cc91cb2645664e1c046f74e8ad4
Borislove <abirme@yandex.ru>
2022-06-23 18:42:29 +0300

    example long

---
+++ src/chapter_3/Light.java
@@ -0,0 +1,26 @@
+package chapter_3;
+
+//Вычислить расстояние, проходимое светом, используя переменные типа long
+public class Light {
+
+    public static void main(String[] args) {
+        int lightspeed;
+        long days;
+        long seconds;
+        long distance;
+
+        //приблизительная скорость света, миль в секунду
+        lightspeed = 186000;
+
+        days = 1000; //указать кол-во дней
+
+        seconds = days * 24 * 60 * 60;        //преобразовать в секунды
+
+        distance = lightspeed * seconds;  //вычислить расстояние
+
+        System.out.print("За " + days);
+        System.out.print(" дней свет пройдет около ");
+        System.out.println(distance + " миль.");
+
+    }
+}
