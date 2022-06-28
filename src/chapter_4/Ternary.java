bceeaa4b112016f7624137622c8d809ce0cb233b
Borislove <abirme@yandex.ru>
2022-06-28 11:24:01 +0300

    тернарная операция ?

---
+++ src/chapter_4/Ternary.java
@@ -0,0 +1,19 @@
+package chapter_4;
+
+//Продемонстрировать применение тернарной операции ?
+public class Ternary {
+    public static void main(String[] args) {
+        int i, k;
+        i = 10;
+        k = i < 0 ? -i : i; //получить абсолютное значение переменной i
+
+        System.out.println("Абсолютное значение ");
+        System.out.println(i + " равно " + k);
+
+        i = -10;
+
+        k = i < 0 ? -i : i; //получить абсолютное значение переменной i
+        System.out.println("Абсолютное значение ");
+        System.out.println(i + " равно " + k);
+    }
+}
