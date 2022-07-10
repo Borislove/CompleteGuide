5b8e0e110e157b1a60af085dcbb89697bbb02139
Borislove <abirme@yandex.ru>
2022-07-10 15:45:35 +0300

    переменная из суперкласса ссылается на объект подкласса

---
+++ src/chapter_8/RefDemo.java
@@ -0,0 +1,19 @@
+package chapter_8;
+
+public class RefDemo {
+    public static void main(String[] args) {
+        BoxWeight weightbox = new BoxWeight(3,5,7,8.37);
+        Box plainbox = new Box();
+        double vol;
+
+        vol = weightbox.volume();
+        System.out.println("Объем weightbox равен "+vol);
+        System.out.println("Вес weightbox раве "+weightbox.weght);
+        System.out.println();
+
+        //присвоить перменной ссылки на объект типа BoxWeight ссылку на объект типа Box
+        plainbox = weightbox;
+        vol = plainbox.volume();//верно, так как метод volume() опеределен в классе Box
+        System.out.println("Объем plainbox равен "+vol);
+    }
+}
