0286b12bf7c52529200d2f91b200a5316916c0d2
Borislove <abirme@yandex.ru>
2022-07-23 12:39:47 +0300

    example stack

---
+++ src/chapter_9/Stack/IntStack.java
@@ -0,0 +1,8 @@
+package chapter_9.Stack;
+
+//Определить интерфейс для целочисленного стека
+public interface IntStack {
+    void push(int item); //сохранить элемент в стеке
+
+    int pop(); //извлечь элемент из стека
+}
