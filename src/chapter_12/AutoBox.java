12d330e292452bd121946a52768c45d1c2da2461
Borislove <abirme@yandex.ru>
2022-08-21 13:38:54 +0300

    автоупаковка

---
+++ src/chapter_12/AutoBox.java
@@ -0,0 +1,13 @@
+package chapter_12;
+
+//Продемонстрировать автоупаковку/автораспаковку
+public class AutoBox {
+    public static void main(String[] args) {
+
+        Integer iOb = 100; //автоупаковка значения типа int
+
+        int i = iOb; //автораспаковка значения типа int
+
+        System.out.println(i + " " + iOb);
+    }
+}
