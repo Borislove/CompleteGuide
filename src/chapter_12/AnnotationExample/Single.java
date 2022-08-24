a8d3816f56ab97517c9096a56e6f76e9996a65fd
Borislove <abirme@yandex.ru>
2022-08-24 14:26:37 +0300

    (Аннотации) Одночленные аннотации

---
+++ src/chapter_12/AnnotationExample/Single.java
@@ -0,0 +1,31 @@
+package chapter_12.AnnotationExample;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.reflect.Method;
+
+//одночленная аннотация
+@Retention(RetentionPolicy.RUNTIME)
+@interface MySingle{
+    int value(); //эта переменная должна иметь имя value
+}
+public class Single {
+    //аннотировать метод одночленной аннотацией
+    @MySingle(100)
+    public static void myMeth(){
+        Single ob = new Single();
+
+        try{
+            Method m = ob.getClass().getMethod("myMeth");
+            MySingle anno = m.getAnnotation(MySingle.class);
+            //выводит значение 100
+            System.out.println(anno.value());
+        }catch (NoSuchMethodException exc){
+            System.out.println("Метод не найден.");
+        }
+    }
+
+    public static void main(String[] args) {
+        myMeth();
+    }
+}
