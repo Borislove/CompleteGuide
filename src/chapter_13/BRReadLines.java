6184d8b96c57dd4d86034f8a61343e1d0a77dc9b
Borislove <abirme@yandex.ru>
2022-08-25 21:00:37 +0300

    чтение символьных строк

---
+++ src/chapter_13/BRReadLines.java
@@ -0,0 +1,21 @@
+package chapter_13;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//Чтение символьных строк с консоли средставми класса BufferedReader
+public class BRReadLines {
+    public static void main(String[] args) throws IOException {
+        //создать поток ввода типа BufferedReader,
+        //используя стандартный поток ввода System.in
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        System.out.println("Введите строки текста.");
+        System.out.println("Введите 'стоп' для завершения.");
+        do {
+            str = br.readLine();
+            System.out.println(str);
+        } while (!str.equals("стоп"));
+    }
+}
