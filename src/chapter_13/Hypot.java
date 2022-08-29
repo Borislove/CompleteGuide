ef81a38181a8b4b53e2997f5116f0981dccea169
Borislove <abirme@yandex.ru>
2022-08-29 19:06:40 +0300

    static import

---
+++ src/chapter_13/Hypot.java
@@ -0,0 +1,20 @@
+package chapter_13;
+
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
+//Вычислить длину гипотенузы прямоугольного треугольника
+//Воспользоваться статическим импортом для доступа к встроенным в Java методам sqrt() и pow()
+public class Hypot {
+    public static void main(String[] args) {
+        double side1, side2;
+        double hypot;
+        side1 = 3.0;
+        side2 = 4.0;
+
+        //Здесь методы sqrt() и pow() можно вызывать непосредственнно, опуская имя их класса
+        hypot = sqrt(pow(side1, 2)) + pow(side2, 2);
+
+        System.out.println("При заданной длине сторон " + side1 + " и " + side2 + " гипотенуза равна " + hypot);
+    }
+}
